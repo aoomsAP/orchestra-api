cd00ee56aba469f3aea5f6c7b9636f4281269fd0
Alki Ooms <alki.ooms@student.ap.be>
2024-05-11 17:28:11 +0200

    Country code as primary key

--- Project/Entities/Country.cs
+++ Project/Entities/Country.cs
@@ -1,22 +1,13 @@
-﻿namespace Project.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Entities
 {
     public class Country
     {
-        // Entity Framework returns empty list as null
-        // this results in a NullReferenceException when attempting to loop through the list
-        // an obvious choice would be to do a null-check before looping through the list
-        // 
-        // therefore it is advised to initialize the list in the constructor of the entity
-        // https://stackoverflow.com/questions/9246729/why-does-entity-framework-return-null-list-instead-of-empty-ones
-        //public Country()
-        //{
-        //    Orchestras = new List<Orchestra>();
-        //}
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-
+        [Key]
         public string Code { get; set; }
+
+        public string Name { get; set; }
 
         public List<Orchestra> Orchestras { get; set; }
     }
--- Project/ViewModels/CountryViewModels.cs
+++ Project/ViewModels/CountryViewModels.cs
@@ -4,20 +4,18 @@ namespace Project.ViewModels
 {
     public class CountryGetViewModel
     {
-        public int Id { get; set; } 
-        public string Name { get; set; }
-
         public string Code { get; set; }
+
+        public string Name { get; set; }
     }
 
     public class CountryCreateViewModel
     {
-        [Required]
-        public string Name { get; set; }
-
         [Required, MaxLength(2), RegularExpression("^[A-Z]{2}$")]
         public string Code { get; set; }
 
+        [Required]
+        public string Name { get; set; }
     }
 
     public class CountryUpdateViewModel
